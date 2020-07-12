7c03635447d66029816cff5d06d0a8c1eb1ff33d
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2020-07-12 14:56:48 -0300

    Fixing typo for Item documentation [ref #6]

--- src/migration/item.go
+++ src/migration/item.go
@@ -1,6 +1,6 @@
 package migration
 
-//Item represents a entry point for an Item entity
+//Item represents a migration (file and url) entry point for an Item entity
 type Item struct {
 	Title      string   `json:"title"`
 	Subtitle   string   `json:"subtitle"`

04b63eda250d549ccf04d89d35cae1ba1ed60677
Diana Lin <lindiana1206@gmail.com>
2020-08-23 20:48:45 -0400

    fix: convert mock payload to json format. refs #16

--- internal/api/v1/routes.go
+++ internal/api/v1/routes.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/bancodobrasil/stop-analyzing-api/internal/db"
@@ -31,37 +32,52 @@ func (c *Controller) Index(ctx *gin.Context) {
 //Choice .
 func (c *Controller) Choice(ctx *gin.Context) {
 
+	// countStr, hasCount := ctx.GetQuery("count")
+
+	// var count int
+
+	// if hasCount {
+	// 	var err error
+
+	// 	count, err = strconv.Atoi(countStr)
+	// 	if err == nil {
+	// 		count = 2
+	// 	}
+	// }
+
 	// Placeholder - Replace with algorithm
 
-	payload :=
-		[]byte(`{
-	"completeness": "0.3",
-	"choices": [
-		{
-			"id": "5f355a24ccb4180025ee98ab",
-			"title": "Fashion Shirt",
-			"subtitle": "Colored nice Shirt",
-			"contentURL": "https://lorempixel.com/640/480/",
-			"attributes": [
+	payload := (json.RawMessage)(
+		`{
+			"completeness": "0.3",
+			"choices": [
 				{
-					"size": "S",
-					"color": "multiple"
+					"id": "5f355a24ccb4180025ee98ab",
+					"title": "Fashion Shirt",
+					"subtitle": "Colored nice Shirt",
+					"contentURL": "https://lorempixel.com/640/480/",
+					"attributes": [
+						{
+							"size": "S",
+							"color": "multiple"
+						}
+					]
+				},
+				{
+					"id": "5f358359ccb4180025ee98ad",
+					"title": "Corporate Shirt",
+					"subtitle": "Corporate long sleeve Shirt",
+					"contentURL": "https://lorempixel.com/640/480/",
+					"attributes": [
+						{
+							"size": "S",
+							"color": "black"
+						}
+					]
 				}
 			]
-		},
-		{
-			"id": "5f358359ccb4180025ee98ad",
-			"title": "Corporate Shirt",
-			"subtitle": "Corporate long sleeve Shirt",
-			"contentURL": "https://lorempixel.com/640/480/",
-			"attributes": [
-				{
-					"size": "S",
-					"color": "black"
-				}
-			]
-		}
-	]
-}`)
-	ctx.Data(http.StatusOK, "application/json", payload)
+		}`,
+	)
+
+	ctx.JSON(http.StatusOK, payload)
 }
